0dcfdcd6a17ed5b11608f7b1e196cd22f218956c
Taucesauce <taucesauce@gmail.com>
2015-05-05 11:56:43 -0500

    Title added and end menu implemented! Now loops endlessly for player enjoyment and accessibility.

---
+++ HoverSlugAdventures/Assets/Scripts/ObjectScripts/EndMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndMenu : MonoBehaviour {
+
+    public void RestartGame()
+    {
+        Application.LoadLevel("Level1");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
