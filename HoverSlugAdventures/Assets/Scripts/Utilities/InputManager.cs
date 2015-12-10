5752810f2946489b2fdc6da77b2bea6ae86892ff
Justin Terry <justin.terry@loop.colum.edu>
2015-12-09 21:11:29 -0600

    Long time no see Hoverslut, you now have touch controls on Surface. You're welcome world.

--- HoverSlugAdventures/Assets/Scripts/Utilities/InputManager.cs
+++ HoverSlugAdventures/Assets/Scripts/Utilities/InputManager.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 namespace Assets.Scripts.Utilities
 {
     public class InputHandler {
+        Vector3 startPos;
+        Vector3 currentPos;
+        Vector3 distance;
+
         public Player.PlayerDirection checkInput()
         {
             //Keyboard Controls
@@ -40,6 +44,54 @@ namespace Assets.Scripts.Utilities
             if (Input.GetKeyDown(KeyCode.Escape))
             {
                 Application.Quit();
+            }
+
+            //Mouse/Swipe Controls
+
+            if(Input.GetMouseButtonDown(0))
+            {
+                startPos = Input.mousePosition;
+                Debug.Log("Just clicked!");
+                return 0;
+            }
+            else if(Input.GetMouseButton(0)) 
+            {
+                currentPos = Input.mousePosition;
+                distance = currentPos - startPos; 
+                return 0;
+            }
+            else if(Input.GetMouseButtonUp(0))
+            {
+                if(Mathf.Abs(distance.x) > Mathf.Abs(distance.y)) //Arbitrary magnitude, needs to relate to screen size but w/e.
+                {
+                    Debug.Log("X Distance times 3: " + distance.x*3);
+                    if(Mathf.Abs(distance.x)*3 > Screen.width)
+                    {
+                        if (distance.x > 0)
+                        {
+                            return Player.PlayerDirection.Right;
+                        }
+                        else if (distance.x < 0)
+                        {
+                            return Player.PlayerDirection.Left;
+                        }
+                    }
+                }
+                else
+                {
+                    Debug.Log("Y Distance times 3: " + distance.y * 3);
+                    if(Mathf.Abs(distance.y)*3 > Screen.height)
+                    {
+                        if (distance.y > 0)
+                        {
+                            return Player.PlayerDirection.Up;
+                        }
+                        else if (distance.y < 0)
+                        {
+                            return Player.PlayerDirection.Down;
+                        }
+                    }
+                }
             }
 
             return 0;
